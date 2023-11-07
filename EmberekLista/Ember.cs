ec0a118fcc6e2f433b0bf395996f476823e10668
Petrik Diak <diak@petrik.hu>
2023-11-07 11:27:42 +0100

    Add project files.

---
+++ EmberekLista/Ember.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmberekLista
+{
+	class Ember
+	{
+		private string nev;
+		private int kor;
+
+		public Ember(string nev, int kor)
+		{
+			this.nev = nev;
+			this.kor = kor;
+		}
+
+		public string Nev { get => nev; set => nev = value; }
+		public int Kor { get => kor; set => kor = value; }
+
+		public override string ToString()
+		{
+			return $"{this.nev, -30} {this.kor, 3} éves";
+		}
+	}
+}
---
+++ EmberekLista/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EmberekLista
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		List<Ember> lista = new List<Ember>();
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			lista.Add(new Ember("Feri", 50));
+			lista.Add(new Ember("Gyuri", 47));
+			lista.Add(new Ember("Pál", 51));
+
+			emberek.ItemsSource = lista;
+		}
+
+		private void addBtn_Click(object sender, RoutedEventArgs e)
+		{
+			if(!string.IsNullOrWhiteSpace(nevTextBox.Text) && !string.IsNullOrWhiteSpace(korTextBox.Text))
+			{
+				try
+				{
+					Convert.ToInt32(korTextBox.Text);
+				}
+				catch (Exception)
+				{
+					MessageBox.Show("");
+					throw;
+				}
+				lista.Add(new Ember(nevTextBox.Text, Convert.ToInt32(korTextBox.Text)));
+				emberek.ItemsSource = lista;
+			}
+		}
+	}
+}
